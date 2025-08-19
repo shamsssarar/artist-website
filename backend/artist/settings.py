a02ec581481c4a91d007e0be844275e2f52300d1
shamsssarar <shamsssarar@gmail.com>
2025-08-19 12:29:12 +0600

    settings updated

--- backend/artist/settings.py
+++ backend/artist/settings.py
@@ -106,7 +106,7 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "NAME": os.getenv("SQLITE_PATH", "/var/data/db.sqlite3"),
         }
     }
 
