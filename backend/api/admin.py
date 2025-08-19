754586345a3486e31b0bb9ef40512ec2d8ec8534
shamsssarar <shamsssarar@gmail.com>
2025-08-19 14:19:03 +0600

    changed dash name

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Contact
 # Register your models here.
 
+admin.site.site_header = "Shaira's Dash"
+admin.site.site_title = "Shaira's Dash"
+admin.site.index_title = "Welcome to Shaira's Dashboard"
+
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display =('id', 'name', 'email', 'created_at')
